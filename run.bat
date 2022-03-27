1acb8988be0c055f4ecb09fb28f664bd9533c11d
Lenovo <dieter.vanderelst@fastmail.net>
2022-03-27 19:00:09 -0400

    first commit

---
+++ run.bat
@@ -0,0 +1,3 @@
+call C:\Users\lab\anaconda3\Scripts\activate.bat C:\Users\lab\anaconda3
+call activate tracking
+call python vr_data_server.py
---
+++ script_generate_shortcut.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 15:28:50 2020
+
+@author: LabUser
+"""
+#https://github.com/newville/pyshortcuts
+from pyshortcuts import make_shortcut
+make_shortcut('C:\\Users\\lab\\Dropbox\\share_with_lab_dropbox\\vr_code\\run.bat', 
+              name='vr_data_server',
+              startmenu=False)
